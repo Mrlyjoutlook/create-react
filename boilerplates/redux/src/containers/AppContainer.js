43dc79f98be483f71b28461d5060f1bef212bac8
Mrlyjoutlook <mr.lyj@outlook.com>
2017-05-23 23:26:26 +0800

    添加异步加载案例

--- boilerplates/redux/src/containers/AppContainer.js
+++ boilerplates/redux/src/containers/AppContainer.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import LazilyLoad, { importLazy } from '../utils/lazilyload';
 
 class AppContainer extends Component {
   static propTypes = {
@@ -11,6 +12,18 @@ class AppContainer extends Component {
 
   shouldComponentUpdate () {
     return false;
+  }
+
+  load = () => {
+    return (
+      <LazilyLoad modules={{
+        Header: () => importLazy(import('../components/Header')),
+      }}>
+        {({ Header }) => (
+          <Header />
+        )}
+      </LazilyLoad>
+    );
   }
 
   render () {
@@ -28,7 +41,7 @@ class AppContainer extends Component {
             <hr />
             <Switch>
               <Route exact path="/" render={() => { console.log('home1'); return (<h1>Home1</h1>); }} />
-              <Route path="/about" render={() => { console.log('home2'); return (<h1>Home2</h1>); }} />
+              <Route path="/about" render={this.load} />
               { /*<Route path="/:id" render={() => { console.log('home3'); return (<h1>Home3</h1>); }} /> */}
               <Route render={() => { console.log('Not Found'); return (<h1>Not Found</h1>); }} />
             </Switch>
